299a4737f477c4afdb502f1fcaf445a49926686e
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-03-30 13:00:44 +0530

    comment setting

--- resources/views/superadmin/academics/view_class.blade.php
+++ resources/views/superadmin/academics/view_class.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.main')
 @section('content')
-<style>
-    .button-container {
-        display: flex;
-        justify-content: flex-end;
-    }
-</style>
+    <style>
+        .button-container {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
     <div class="right_col" role="main">
         <div class="">
             <div class="page-title">
@@ -34,12 +34,6 @@
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
                                 <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                        aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item" href="#">Settings 1</a>
-                                        <a class="dropdown-item" href="#">Settings 2</a>
-                                    </div>
                                 </li>
                                 <li><a class="close-link"><i class="fa fa-close"></i></a>
                                 </li>
@@ -50,11 +44,12 @@
                         <div class="x_content">
                             <div class="table-responsive">
                                 <div class="button-container">
-                                    <a href="{{ route('create.class') }}"><button type="button" class="btn btn-primary btn-sm mb-2">Add Class</button></a>
+                                    <a href="{{ route('create.class') }}"><button type="button"
+                                            class="btn btn-primary btn-sm mb-2">Add Class</button></a>
                                 </div>
                                 <table class="table table-striped jambo_table bulk_action" id="table">
                                     <thead>
-                                          <tr class="">
+                                        <tr class="">
                                             <th>No</th>
                                             <th class="">Class</th>
                                             <th class="">Section</th>
@@ -64,14 +59,16 @@
 
                                     <tbody>
                                         @foreach ($classes as $index => $cla)
-                                        <tr class="">
-                                          <td>{{ $index + 1 }}</td>
-                                          <td class="">{{ $cla->class }}</td>                
-                                            <td class="">{{ $cla->section }}</td>                
-                                                <td> 
-                                                    <a href="{{ route('edit.class', $cla->id) }}" class="btn btn-info btn-sm">Edit</a>
+                                            <tr class="">
+                                                <td>{{ $index + 1 }}</td>
+                                                <td class="">{{ $cla->class }}</td>
+                                                <td class="">{{ $cla->section }}</td>
+                                                <td>
+                                                    <a href="{{ route('edit.class', $cla->id) }}"
+                                                        class="btn btn-info btn-sm">Edit</a>
 
-                                                    <a href="{{ route('destroy',$cla->id) }}" class="btn btn-danger btn-sm">Delete</a>
+                                                    <a href="{{ route('destroy', $cla->id) }}"
+                                                        class="btn btn-danger btn-sm">Delete</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -86,10 +83,10 @@
     </div>
 @endsection
 @push('scripts')
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script>
-    $(document).ready(function () {
-        $('#table').DataTable();
-    });
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
     </script>
 @endpush
--- resources/views/superadmin/academics/view_teacher.blade.php
+++ resources/views/superadmin/academics/view_teacher.blade.php
@@ -1,11 +1,105 @@
 @extends('admin.main')
 @section('content')
-<style>
-    .button-container {
-        display: flex;
-        justify-content: flex-end;
-    }
-</style>
+    <style>
+        .button-container {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>Teacher List</h3>
+                </div>
+                <div class="title_right">
+                    <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="row" style="display: block;">
+                <div class="clearfix"></div>
+                <div class="col-md-12 col-sm-12  ">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Table</h2>
+                            <ul class="nav navbar-right panel_toolbox">
+                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                </li>
+                                <li class="dropdown">
+
+                                </li>
+                                <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                </li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="x_content">
+                            <div class="table-responsive">
+                                <div class="button-container">
+                                    <a href="{{ route('create.teacher') }}"><button type="button"
+                                            class="btn btn-primary btn-sm mb-2">Add Teacher</button></a>
+                                </div>
+                                <table class="table table-striped jambo_table bulk_action" id="table">
+                                    <thead>
+                                        <tr class="">
+                                            <th>No</th>
+                                            <th class="">Teacher List</th>
+                                            <th class=""><span class="nobr">Action</span></th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach ($teachers as $index => $teacher)
+                                            <tr class="">
+                                                <td>{{ $index + 1 }}</td>
+                                                <td class="">{{ $teacher->name }}</td>
+                                                <td>
+                                                    <a href="{{ route('edit.teacher', $teacher->id) }}"
+                                                        class="btn btn-info btn-sm">Edit</a>
+
+                                                    <a href="{{ route('destroy.teacher', $teacher->id) }}"
+                                                        class="btn btn-danger btn-sm">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@push('scripts')
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
+@endpush
+<<<<<<< Updated upstream
+
+=======
+@extends('admin.main')
+@section('content')
+    <style>
+        .button-container {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
     <div class="right_col" role="main">
         <div class="">
             <div class="page-title">
@@ -50,26 +144,29 @@
                         <div class="x_content">
                             <div class="table-responsive">
                                 <div class="button-container">
-                                    <a href="{{ route('create.teacher') }}"><button type="button" class="btn btn-primary btn-sm mb-2">Add Teacher</button></a>
+                                    <a href="{{ route('create.teacher') }}"><button type="button"
+                                            class="btn btn-primary btn-sm mb-2">Add Teacher</button></a>
                                 </div>
                                 <table class="table table-striped jambo_table bulk_action" id="table">
                                     <thead>
-                                          <tr class="">
+                                        <tr class="">
                                             <th>No</th>
-                                            <th class="">Teacher List</th>
+                                            <th class="">Section</th>
                                             <th class=""><span class="nobr">Action</span></th>
                                         </tr>
                                     </thead>
 
                                     <tbody>
                                         @foreach ($teachers as $index => $teacher)
-                                        <tr class="">
-                                          <td>{{ $index + 1 }}</td>
-                                            <td class="">{{ $teacher->name }}</td>                
-                                                <td> 
-                                                    <a href="{{ route('edit.teacher', $teacher->id) }}" class="btn btn-info btn-sm">Edit</a>
+                                            <tr class="">
+                                                <td>{{ $index + 1 }}</td>
+                                                <td class="">{{ $teacher->name }}</td>
+                                                <td>
+                                                    <a href="{{ route('edit.teacher', $teacher->id) }}"
+                                                        class="btn btn-info btn-sm">Edit</a>
 
-                                                    <a href="{{ route('destroy.teacher',$teacher->id) }}" class="btn btn-danger btn-sm">Delete</a>
+                                                    <a href="{{ route('destroy', $teacher->id) }}"
+                                                        class="btn btn-danger btn-sm">Delete</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -84,11 +181,11 @@
     </div>
 @endsection
 @push('scripts')
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script>
-    $(document).ready(function () {
-        $('#table').DataTable();
-    });
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
     </script>
 @endpush
-
+>>>>>>> Stashed changes
